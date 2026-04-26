af6274e934ae67d5ad59c14d985894fdc4d8ab48
SujalPawar <pawarsujal4455@gmail.com>
2026-04-26 13:38:46 +0530

    Update file

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ import streamlit as st
 import mysql.connector
 from PyPDF2 import PdfReader
 from openai import OpenAI
+
+client= OpenAI(api_key="sk-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+def db():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",        
+        database="notes_db"
+    )
